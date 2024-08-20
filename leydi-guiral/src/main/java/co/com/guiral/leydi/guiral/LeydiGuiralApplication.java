4d47d5784bfa72568616f3f2bf5833b5591031a2
Leydi09Guiral <113058743+Leydi09Guiral@users.noreply.github.com>
2024-08-20 18:52:07 -0500

    Add files via upload

---
+++ leydi-guiral/src/main/java/co/com/guiral/leydi/guiral/LeydiGuiralApplication.java
@@ -0,0 +1,13 @@
+package co.com.guiral.leydi.guiral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeydiGuiralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeydiGuiralApplication.class, args);
+	}
+
+}
